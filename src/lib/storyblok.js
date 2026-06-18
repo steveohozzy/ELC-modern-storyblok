b3c52ecf65e54f18652d004ed93c5d875b6837ef
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2026-06-18 09:05:54 +0100

    added marquee component

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -1,16 +1,22 @@
 import Page from "@/components/Page";
 import HomepageHero from "@/components/HomepageHero";
 import StorySection from "@/components/StorySection";
+import Marquee from "@/components/Marquee";
+
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
 
 export const getStoryblokApi = storyblokInit({
   accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
+
   use: [apiPlugin],
+
   components: {
     page: Page,
     homepageHero: HomepageHero,
     storySection: StorySection,
+    Marquee: Marquee,
   },
+
   apiOptions: {
     region: "eu",
   },
